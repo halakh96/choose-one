7c9a02916195d5c761a584ed99df39c34358a2e3
Halakh96 <halakhaled96@hotmail.com>
2022-03-08 09:09:25 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
-import LogginPage from "./pages/LogginPage";
+import Loggin from "./pages/Loggin";
 import AddQuestion from "./pages/AddQuestion";
 import LeaderBoard from "./pages/LeaderBoard";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
 
 const App = () => {
+  const activeUser = useSelector((state) => state.activeUser);
+
+  useEffect(() => {
+    if (activeUser.length == 0) {
+      return <Navigate to="/" />;
+    }
+  }, []);
+
   return (
     <div>
       <Routes>
-        <Route exact path="/" element={<LogginPage />} />
+        <Route exact path="/" element={<Loggin />} />
         <Route path="/Home" element={<Home />} />
         <Route path="/LeaderBoard" element={<LeaderBoard />} />
         <Route path="/AddQuestion" element={<AddQuestion />} />
