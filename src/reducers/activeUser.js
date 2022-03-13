d3f3c7f3c87a99cae20d6dbc5ab5f8b01b566f50
Halakh96 <halakhaled96@hotmail.com>
2022-03-14 00:42:27 +0300

    Updates

--- src/reducers/activeUser.js
+++
@@ -1,9 +0,0 @@
-import { ACTIVE_USER } from "../actions/types";
-
-export default function activeUser(state = [], action) {
-  if (action.type === ACTIVE_USER) {
-    return { ...action.user };
-  } else {
-    return state;
-  }
-}
--- src/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from "redux";
-import users from "./users";
-import questions from "./questions";
-import activeUser from "./activeUser";
-
-export const rootReducer = combineReducers({ users, questions, activeUser });
